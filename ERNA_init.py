a73b502b6b19308aa0781cc6ebf7d33d35798a64
gzapodea <gzapodea@cisco.com>
2017-04-17 17:24:30 -0700

    Ready for Cisco Live US

--- ERNA_init.py
+++ ERNA_init.py
@@ -2,6 +2,8 @@
 
 # !/usr/bin/env python3
 
+# This file contains the Spark Auth and Tropo key for each user
+
 SPARK_AUTH = 'Bearer ' + 'Put your Spark token here'
 
 TROPO_KEY = 'Put your Tropo key here'
